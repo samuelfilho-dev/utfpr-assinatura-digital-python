c9ccd316e44ed5b47103fe90f04e84bd9667345f
samuel-filho-c9 <samuel@c9digital.com.br>
2025-05-07 18:36:53 -0300

    feat: Add Docker Compose configuration for Redis and RedisInsight services

---
+++ verificar_assinatura_view.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from main_controller import verificar_assinatura_pdf
+
+
+def verificar_assinatura_view(root):
+    """
+    Função para criar a view de assinatura digital.
+    :param root: Instância da janela principal do Tkinter.
+    :return: None
+    """
+    botao_verficar = tk.Button(root, text="Verificar Assinatura", command=verificar_assinatura_pdf)
+    botao_verficar.pack(pady=20)
+    return botao_verficar
